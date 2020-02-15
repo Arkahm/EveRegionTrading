6f5f13f9618be8bbc56dafdc4dd40f08ad9c31de
arkahm <gilmore090870@gmail.com>
2020-02-15 12:39:46 -0800

    made DataFrame to capture min/max prices

--- Market2.py
+++ Market2.py
@@ -29,7 +29,7 @@ print(start)
 url_station = 'https://esi.evetech.net/latest/markets/' + \
     str(Forge) + '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = 5  # region_List.headers['x-pages']
+no_pages = region_List.headers['x-pages']
 jitaOrders = get_orders(Forge, Jita_location_id, int(no_pages))
 lowestJita = getLowest(jitaOrders, Jita_location_id)
 print(datetime.today() - start)
@@ -37,7 +37,7 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + str(Domain) + \
     '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = 5  # region_List.headers['x-pages']
+no_pages = region_List.headers['x-pages']
 amarrOrders = get_orders(Domain, Amarr_location_id, int(no_pages))
 lowestAmarr = getLowest(amarrOrders, Amarr_location_id)
 print(datetime.today() - start)
@@ -45,7 +45,7 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + \
     str(SinqLaison) + '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = 5  # region_List.headers['x-pages']
+no_pages = region_List.headers['x-pages']
 dodiOrders = get_orders(SinqLaison, Dodixie_location_id, int(no_pages))
 lowestDodi = getLowest(dodiOrders, Dodixie_location_id)
 print(datetime.today() - start)
@@ -53,7 +53,7 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + str(Metropolis) + \
     '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = 5  # region_List.headers['x-pages']
+no_pages = region_List.headers['x-pages']
 hekOrders = get_orders(Metropolis, Hek_location_id, int(no_pages))
 lowestHek = getLowest(hekOrders, Hek_location_id)
 print(datetime.today() - start)
@@ -61,7 +61,7 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + str(Heimatar) + \
     '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = 5  # region_List.headers['x-pages']
+no_pages = region_List.headers['x-pages']
 rensOrders = get_orders(Heimatar, Rens_location_id, int(no_pages))
 lowestRens = getLowest(rensOrders, Rens_location_id)
 print(datetime.today() - start)
