b7ec71711c295f8cbc2573b825643ec5efdc1639
arkahm <gilmore090870@gmail.com>
2020-02-17 18:28:42 -0800

    minor changes to groupby code

--- Market2.py
+++ Market2.py
@@ -29,7 +29,7 @@ print(start)
 url_station = 'https://esi.evetech.net/latest/markets/' + \
     str(Forge) + '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = region_List.headers['x-pages']
+no_pages = 10  # region_List.headers['x-pages']
 jitaOrders = get_orders(Forge, Jita_location_id, int(no_pages))
 lowestJita = getLowest(jitaOrders, Jita_location_id)
 print(datetime.today() - start)
@@ -37,7 +37,7 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + str(Domain) + \
     '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = region_List.headers['x-pages']
+no_pages = 10  # region_List.headers['x-pages']
 amarrOrders = get_orders(Domain, Amarr_location_id, int(no_pages))
 lowestAmarr = getLowest(amarrOrders, Amarr_location_id)
 print(datetime.today() - start)
@@ -45,7 +45,7 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + \
     str(SinqLaison) + '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = region_List.headers['x-pages']
+no_pages = 10  # region_List.headers['x-pages']
 dodiOrders = get_orders(SinqLaison, Dodixie_location_id, int(no_pages))
 lowestDodi = getLowest(dodiOrders, Dodixie_location_id)
 print(datetime.today() - start)
@@ -53,7 +53,7 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + str(Metropolis) + \
     '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = region_List.headers['x-pages']
+no_pages = 10  # region_List.headers['x-pages']
 hekOrders = get_orders(Metropolis, Hek_location_id, int(no_pages))
 lowestHek = getLowest(hekOrders, Hek_location_id)
 print(datetime.today() - start)
@@ -61,25 +61,43 @@ print(datetime.today() - start)
 url_station = 'https://esi.evetech.net/latest/markets/' + str(Heimatar) + \
     '/orders/?datasource=tranquility&order_type=sell'
 region_List = requests.get(url_station)
-no_pages = region_List.headers['x-pages']
+no_pages = 10  # region_List.headers['x-pages']
 rensOrders = get_orders(Heimatar, Rens_location_id, int(no_pages))
 lowestRens = getLowest(rensOrders, Rens_location_id)
 print(datetime.today() - start)
 
-# combines individual staqtion list into one large market list
+# combines individual station list into one large market list
 lowest_highest = lowestJita + lowestRens + lowestDodi + lowestAmarr + lowestHek
 
 df2 = pd.DataFrame(lowest_highest)
-# df2 = df2.set_index(['duration'])
 df2 = df2.sort_values(by=['type_id'])
 df2.to_csv(r'market_working_files/combined.csv')
 
-print(df2)  # for testing purposes only. It is not needed.
+replace_location = {
+    60008494: 'Amarr',
+    60005686: 'Hek',
+    60003760: 'Jita',
+    60004588: 'Rens',
+    60011866: 'Dodixie'
+}
+
+df2.replace(replace_location, inplace=True)
+
+# for testing purposes only. It is not needed.
+# print(df2.head(20))
 
 # groups type id's into each type and gets min/max price
 typeid_grp = df2.groupby(['type_id'])
+print(typeid_grp)
 type_group_marg = typeid_grp['price'].agg(['min', 'max'])
-type_group_marg['Margin'] = ((type_group_marg['max']-type_group_marg['min'])/type_group_marg['max'])*100
+print(type_group_marg)
+type_group_marg.rename(columns={'min': 'Buy Price', 'max': 'Sell Price'}, inplace=True)
+type_group_marg['Margin'] = ((type_group_marg['Sell Price'] -
+                             type_group_marg['Buy Price'])/type_group_marg['Sell Price'])*100
+
+# filters DF to only above 35% margin
+type_group_marg = type_group_marg[type_group_marg['Margin'] >= 40]
+
 print(type_group_marg)
 
 print(datetime.today() - start)
